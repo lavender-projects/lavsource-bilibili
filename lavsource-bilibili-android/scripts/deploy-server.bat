d505c5b72e4bc3571cf5ab978199c0759670074d
Kosaka Bun <kosaka-bun@qq.com>
2024-01-16 22:25:41 +0800

    创建server项目和web项目的Linux端部署脚本

--- lavsource-bilibili-android/scripts/deploy-server.bat
+++
@@ -1,9 +0,0 @@
-chcp 65001
-cd /d %~dp0..\lavender-reader-server
-
-set TARGET_DIR=..\app\src\main\assets\inner-data-server
-
-rmdir /s /q .\build\libs
-del /s /q %TARGET_DIR%\inner-data-server.jar
-call gradlew bootJar
-copy .\build\libs\lavender-reader-server.jar %TARGET_DIR%\inner-data-server.jar
