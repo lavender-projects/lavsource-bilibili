c1deceb0bf67128b16d4874eee9fec86d9289bff
Kosaka Bun <kosaka-bun@qq.com>
2024-01-31 16:23:24 +0800

    将项目结构改造为复合gradle项目

---
+++ buildSrc/src/main/java/de/honoka/gradle/buildsrc/Versions.kt
@@ -0,0 +1,22 @@
+package de.honoka.gradle.buildsrc
+
+@Suppress("ConstPropertyName")
+object Versions {
+
+    const val kotlin = "1.6.21"
+
+    const val springBoot = "2.7.5"
+
+    const val honokaUtils = "1.0.8"
+
+    const val honokaFrameworkUtils = "1.0.3"
+
+    const val lavsourceSpringBootStarterVersion = "1.0.1-dev"
+
+    object App {
+
+        const val android = "8.0.0"
+
+        const val kotlin = "1.8.0"
+    }
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,27 @@
+@file:Suppress("UnstableApiUsage")
+
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        mavenLocal()
+        maven("https://maven.aliyun.com/repository/public")
+        google()
+        mavenCentral()
+        maven("https://mirrors.honoka.de/maven-repo")
+    }
+}
+
+pluginManagement {
+    repositories {
+        maven("https://maven.aliyun.com/repository/gradle-plugin")
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+
+rootProject.name = "lavsource-bilibili"
+
+include(":server")
+include(":android")
+include(":android-web")
