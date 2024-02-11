6abd8595fe364e6b5aaff5fb5643f8986834fa2a
Kosaka Bun <kosaka-bun@qq.com>
2024-02-11 11:19:23 +0800

    Android项目适配BaseContentProvider

--- android/src/main/java/de/honoka/lavender/lavsource/android/provider/LavsourceProvider.kt
+++ android/src/main/java/de/honoka/lavender/lavsource/android/provider/LavsourceProvider.kt
@@ -1,36 +1,12 @@
 package de.honoka.lavender.lavsource.android.provider
 
-import android.content.ContentProvider
-import android.content.ContentValues
-import android.database.Cursor
-import android.net.Uri
-import android.os.Bundle
+import cn.hutool.json.JSON
+import cn.hutool.json.JSONObject
+import de.honoka.sdk.util.android.common.BaseContentProvider
 
-class LavsourceProvider : ContentProvider() {
+class LavsourceProvider : BaseContentProvider() {
 
-    override fun call(method: String, arg: String?, extras: Bundle?): Bundle? {
-        return null
+    override fun call(args: JSON?): Any = JSONObject().also {
+        it["msg"] = "hello"
     }
-
-    //region 无关方法
-
-    override fun onCreate(): Boolean = false
-
-    override fun query(
-        uri: Uri, projection: Array<String>?, selection: String?,
-        selectionArgs: Array<String>?, sortOrder: String?
-    ): Cursor? = null
-
-    override fun getType(uri: Uri): String? = null
-
-    override fun insert(uri: Uri, values: ContentValues?): Uri? = null
-
-    override fun update(
-        uri: Uri, values: ContentValues?, selection: String?,
-        selectionArgs: Array<String>?
-    ): Int = 0
-
-    override fun delete(uri: Uri, selection: String?, selectionArgs: Array<String>?): Int = 0
-
-    //endregion
 }
