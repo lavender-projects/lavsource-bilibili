14253378b436c0c3005ad4879da1dc5614ac0e30
Kosaka Bun <kosaka-bun@qq.com>
2024-03-08 15:39:21 +0800

    迁移代码至honoka-android-utils项目中
    迁移BasicJsInterface

--- android/src/main/java/de/honoka/lavender/lavsource/android/ui/MainActivity.kt
+++ android/src/main/java/de/honoka/lavender/lavsource/android/ui/MainActivity.kt
@@ -1,17 +1,14 @@
 package de.honoka.lavender.lavsource.android.ui
 
-import android.content.Intent
 import android.os.Bundle
-import android.view.View
-import android.view.WindowManager
 import androidx.appcompat.app.AppCompatActivity
 import de.honoka.lavender.lavsource.android.controller.bilibiliController
 import de.honoka.lavender.lavsource.bilibili.R
 import de.honoka.sdk.util.android.common.GlobalComponents
+import de.honoka.sdk.util.android.common.launchCoroutineOnIoThread
 import de.honoka.sdk.util.android.server.HttpServer
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
+import de.honoka.sdk.util.android.ui.fullScreenToShow
+import de.honoka.sdk.util.android.ui.jumpToWebActivty
 
 class MainActivity : AppCompatActivity() {
 
@@ -19,25 +16,11 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         fullScreenToShow()
         setContentView(R.layout.activity_main)
-        CoroutineScope(Dispatchers.IO).launch {
+        launchCoroutineOnIoThread {
             //init可能是一个耗时的操作，故在IO线程中执行，防止阻塞UI线程
             initApplication()
-            jumpToWebActivty()
+            jumpToWebActivty(WebActivity::class.java)
         }
-    }
-
-    /**
-     * 全屏化当前Activity
-     */
-    @Suppress("DEPRECATION")
-    private fun fullScreenToShow() {
-        //隐藏状态栏（手机时间、电量等信息显示的地方）
-        window.setFlags(
-            WindowManager.LayoutParams.FLAG_FULLSCREEN,
-            WindowManager.LayoutParams.FLAG_FULLSCREEN
-        )
-        //隐藏虚拟按键
-        window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION or View.SYSTEM_UI_FLAG_FULLSCREEN
     }
 
     private fun initApplication() {
@@ -50,12 +33,5 @@ class MainActivity : AppCompatActivity() {
             bilibiliController
         )
         HttpServer.checkOrRestartInstance()
-    }
-
-    private fun jumpToWebActivty() = runOnUiThread {
-        startActivity(Intent(this, WebActivity::class.java).apply {
-            putExtra("firstWebActivity", true)
-        })
-        finish()
     }
 }
